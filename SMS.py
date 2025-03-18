b286fafa08e11a67590ea2e8ef4e935361647ac7
AbhiramAnish <abhiramanish2016@gmail.com>
2025-03-18 21:05:03 +0530

    sms

--- SMS.py
+++ SMS.py
@@ -1,34 +1,33 @@
+import os
 from twilio.rest import Client
-
 from datetime import datetime
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 class sms:
-    def msg(self,prediction):
-        account_sid = 'AC9064b095ee5db08a3045efe80140b72e'
-        auth_token = '880c31974f1e34fe3726ab1f2db3f802'
-        client = Client(account_sid, auth_token)
+    def __init__(self):
+        self.account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+        self.auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+        self.messaging_service_sid = os.getenv("TWILIO_MESSAGING_SERVICE_SID")
+        self.client = Client(self.account_sid, self.auth_token)
+        self.numbers = ['+918547751321', '+919746458177', '+918137019259']
 
-        # message = client.messages.create(
-        #   messaging_service_sid='MG75dfa40854e9bbb803e81503795cb3f6',
-        #   body='//website',
-        #   to='+918547751321'
-        # )
-        # print(message.sid)
+    def send_msg(self, prediction):
+        if prediction == 1:
+            print("SMS sending...")
+            for number in self.numbers:
+                try:
+                    message = self.client.messages.create(
+                        messaging_service_sid=self.messaging_service_sid,
+                        body=f'Disaster predicted\nWebsite: https://abhiramanish.github.io/disaster/\n{datetime.now()}',
+                        to=number
+                    )
+                    print(f"Message sent to {number}: {message.sid}")
+                except Exception as e:
+                    print(f"Failed to send message to {number}: {str(e)}")
 
-        numbers = ['+918547751321','+919746458177','+918137019259']
-
-        # prediction = 1
-
-        if prediction==1:
-            print("SMS sending")
-            for number in numbers:
-                message = client.messages.create(
-                messaging_service_sid='MG520b43f804e8aa31430192f7036e9edb',
-                body='Disaster predicted\nwebsite: https://abhiramanish.github.io/disaster/\n'+str(datetime.now()),
-                to=number
-                )
-                print(message.sid)
-
-
-# obj = sms()
-# obj.msg(1)
+# Usage Example
+# obj = SMS()
+# obj.send_msg(1)
